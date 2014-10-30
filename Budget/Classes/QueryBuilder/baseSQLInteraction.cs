c8f600bf0b8d61a87168d45bc3528bec657339b5
dannyburrows <burrows.danny@gmail.com>
2014-10-29 20:17:08 -0500

    Added new files.

---
+++ Budget/Classes/QueryBuilder/baseSQLInteraction.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace budgetApp {
+    /* the most basic interface class, other interfaces will inherit from this one */
+    class baseInterface {
+        #region Parameters
+        protected publicEnums.classType className = publicEnums.classType.none;
+        protected queryBuilder query = new queryBuilder();
+        protected List<parameter> inputs = new List<parameter>();
+        protected List<responseRow> rows = null; // set when multiple rows are expected as a result
+        protected responseRow row = null; // set when only one row is expected as a result
+        protected SQLiteDataReader response = null;//new SQLiteDataReader();
+        #endregion
+
+        #region Database Interactions
+        /// <summary>
+        /// Executes a given query, dynamically creating the number of columns and number of rows, adding to this.rows.
+        /// this.rows can be interfaced with at a higher level to create various objects.
+        /// </summary>
+        /// <param name="queryName"></param>
+        /// <returns></returns>
+        protected bool executeGetMultiple(string queryName) {
+            bool success = true;
+            this.rows = new List<responseRow>();
+            List<responseColumn> cols = new List<responseColumn>();
+            
+            this.query.getQueryDetails(queryName);
+            this.query.addParameters(this.inputs);
+            using (this.query.sqlConn) {
+                this.query.openConn();
+                using (this.query.command) {
+                    this.response = this.query.command.ExecuteReader();
+                    try {
+                        while (this.response.Read()) {
+                            for (int i = 0; i < this.response.FieldCount; i++) {
+                                cols.Add(new responseColumn(this.response.GetName(i), this.response[i].ToString()));
+                                //string temp = this.response.GetName(i);
+                            }
+                            this.rows.Add(new responseRow(cols));
+                            cols.Clear();
+                        }
+                    } catch (SQLiteException e) {
+                        MessageBox.Show("An error occured reading from the database.\n\n" + e.ToString());
+                        success = false;
+                    } finally {
+                        /* deconstruct */
+                        this.query.closeConn();
+                        this.inputs.Clear();
+                    }
+                }
+
+            }
+            return success;
+        }
+        /// <summary>
+        /// Executes given query, setting only this.row, as only a single result is expected back.
+        /// </summary>
+        /// <param name="queryName"></param>
+        /// <returns></returns>
+        protected bool executeGetSingle( string queryName ) {
+            bool success = true;
+            this.row = new responseRow();
+            List<responseColumn> cols = new List<responseColumn>();
+
+            this.query.getQueryDetails(queryName);
+            this.query.addParameters(this.inputs);
+
+            using (this.query.sqlConn) {
+                this.query.openConn();
+                using (this.query.command) {
+                    this.response = this.query.command.ExecuteReader();
+                    try {
+                        if (this.response.HasRows) {
+                            for (int i = 0; i < this.response.FieldCount; i++) {
+                                cols.Add(new responseColumn(this.response.GetName(i), this.response[i].ToString()));
+                            }
+                        }
+                        /* Add the columns to the row object */
+                        this.row = new responseRow(cols);
+                    } catch (SQLiteException e) {
+                        MessageBox.Show("Error in reading from database." + e.ToString());
+                        success = false;
+                    } finally {
+                        this.query.closeConn();
+                        this.inputs.Clear();
+                    }
+                }
+            }
+            return success;
+        }
+        /// <summary>
+        /// Used to execute a database operation that is not returning rows (i.e. INSERT, DELETE, UPDATE)
+        /// </summary>
+        /// <param name="queryName"></param>
+        /// <returns>true upon success</returns>
+        protected bool executeNonQuery( string queryName ) {
+            bool success = true;
+            this.query.getQueryDetails(queryName);
+            this.query.addParameters(this.inputs);
+            using (this.query.sqlConn) {
+                this.query.openConn();
+                try {
+                    this.query.command.ExecuteNonQuery();
+                    success = true;
+                } catch (SQLiteException e) {
+                    MessageBox.Show("An error occured writing to the database.\n\n" + e.ToString());
+                    success = false;
+                } finally {
+                    this.inputs.Clear();
+                    this.query.closeConn();
+                }
+            }
+
+            return success;
+        }
+        #endregion
+
+        #region Query Names
+        /// <summary>
+        /// Returns the name of the query responsible for pulling information from the database by the id of the object.
+        /// </summary>
+        /// <returns>String that contains query name</returns>
+        protected string getByIDQuery() {
+            switch (this.className) {
+                case publicEnums.classType.none:
+                    return "error";
+                case publicEnums.classType.account:
+                    return "getAccountByID";
+                case publicEnums.classType.accountCategory:
+                    return "getAccountCategoryByID";
+                case publicEnums.classType.accountRegister:
+                    return "getAccountRegisterByID";
+                case publicEnums.classType.bill:
+                    return "getBillByID";
+                case publicEnums.classType.budget:
+                    return "getBudgetByID";
+                case publicEnums.classType.budgetCategory:
+                    return "getBugdetCategoryByID";
+                case publicEnums.classType.income:
+                    return "getIncomeByID";
+                case publicEnums.classType.ledger:
+                    return "getLedgerByID";
+            }
+            return "error";
+        }
+        /// <summary>
+        /// Returns the name of the query responsible for pulling information from the database via the name of the class.
+        /// </summary>
+        /// <param name="returnList">Determines whether one response or a list of responses is to be expected</param>
+        /// <returns name="queryName">String that contains the query name for getting information from the database based on the name passed to it</returns>
+        protected string getByNameQuery( bool returnList = false ) {
+            string queryName = "error";
+            switch (this.className) {
+                case publicEnums.classType.none:
+                    queryName = "error";
+                    break;
+                case publicEnums.classType.account:
+                    queryName = "getAccountByID";
+                    break;
+                case publicEnums.classType.accountCategory:
+                    queryName = "getAccountCategoryByID";
+                    break;
+                case publicEnums.classType.accountRegister:
+                    queryName = "getAccountRegisterByID";
+                    break;
+                case publicEnums.classType.bill:
+                    queryName = "getBillByID";
+                    break;
+                case publicEnums.classType.budget:
+                    queryName = "getBudgetByID";
+                    break;
+                case publicEnums.classType.budgetCategory:
+                    queryName = "getBugdetCategoryByID";
+                    break;
+                case publicEnums.classType.expense:
+                    queryName = "getExpenseByID";
+                    break;
+                case publicEnums.classType.income:
+                    queryName = "getIncomeByID";
+                    break;
+                case publicEnums.classType.ledger:
+                    queryName = "getLedgerByID";
+                    break;
+            }
+            // different queries for one object vs all of them, append All to make sure a list is going to be returned
+            if (returnList) { queryName = queryName + "All"; }
+            return queryName;
+        }
+        protected string getByDateRange() {
+            switch (this.className) {
+                case publicEnums.classType.none:
+                    return "error";
+                case publicEnums.classType.expense:
+                    return "getExpenseDateRange";
+            }
+            return "error";
+        }
+        protected string getLastID() {
+            switch (this.className) {
+                case publicEnums.classType.expense:
+                    return "getLastExpenseID";
+            }
+            return "error";
+        }
+        protected string insertQuery() {
+            switch (this.className) {
+                case publicEnums.classType.expense:
+                    return "insertExpense";
+                case publicEnums.classType.ledger:
+                    return "insertLedger";
+            }
+            return "error";
+        }
+
+        protected string updateQuery() {
+            switch (this.className) {
+                case publicEnums.classType.ledger:
+                    return "updateLedger";
+            }
+            return "error";
+        }
+        #endregion
+    }
+}
---
+++ Budget/Classes/QueryBuilder/responseRow.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budgetApp {
+    /// <summary>
+    /// Contains a string representation of a single row from a database query. Columns can be added dynamically, depending on the number of columns being return from the query.
+    /// </summary>
+    class responseRow {
+        #region Properties
+        private List<responseColumn> columns = new List<responseColumn>();
+        #endregion
+
+        #region Constructors
+        public responseRow() {
+            // nothing for now
+        }
+        public responseRow(List<responseColumn> cols) {
+            foreach (responseColumn col in cols) {
+                this.columns.Add(col);
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Adds a column to the row
+        /// </summary>
+        /// <param name="colName"></param>
+        /// <param name="colValue"></param>
+        public void addColumn( string colName, string colValue ) {
+            this.columns.Add(new responseColumn(colName, colValue));
+        }
+        /// <summary>
+        /// Adds column to the row
+        /// </summary>
+        /// <param name="col"></param>
+        public void addColumn(responseColumn col) {
+            this.columns.Add(col);
+        }
+        /// <summary>
+        /// Adds all columns in an existing row
+        /// </summary>
+        /// <param name="row"></param>
+        public void addRow( List<responseColumn> row ) {
+            foreach (responseColumn col in row) {
+                this.columns.Add(col);
+            }
+        }
+        /// <summary>
+        /// Gets the integer index of a column name
+        /// </summary>
+        /// <param name="colName"></param>
+        /// <returns>integer</returns>
+        public int getIndexOfColumnName( string colName ) {
+            for (int i = 0; i < this.columns.Count; i++) {
+                if (colName == this.columns[i].columnName) {
+                    return i;
+                }
+            }
+            // did not find, catch the error on the calling procedure
+            return -1;
+        }
+        /// <summary>
+        /// Gets a column by index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public responseColumn getColumn( int index ) {
+            return this.columns[index];
+        }
+        /// <summary>
+        /// Gets a column by its name
+        /// </summary>
+        /// <param name="colName"></param>
+        /// <returns></returns>
+        public responseColumn getColumn( string colName ) {
+            foreach (responseColumn col in this.columns) {
+                if (col.columnName == colName) {
+                    return col;
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// Gets value of column based on the inserted name
+        /// </summary>
+        /// <param name="colName"></param>
+        /// <returns>string with the value of the column with the input column name</returns>
+        public string getColumnValue( string colName ) {
+            foreach (responseColumn col in this.columns) {
+                if (col.columnName == colName) {
+                    return col.columnValue;
+                }
+            }
+            return string.Empty;
+        }
+        /// <summary>
+        /// Returns the entire row (and all the columns in it)
+        /// </summary>
+        /// <returns></returns>
+        public List<responseColumn> getRow() {
+            return this.columns;
+        }
+        #endregion
+    }
+}
